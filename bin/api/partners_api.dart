5d7d3d102ed8e92948a8b95fed031725d44affa8
Lucas Reinaldo <lucazreinaldo@gmail.com>
2022-12-21 01:11:34 -0300

    Criado API de acordo com o padrao Restful

--- bin/api/partners_api.dart
+++ bin/api/partners_api.dart
@@ -4,8 +4,26 @@ import 'package:shelf_router/shelf_router.dart';
 class PartnersApi {
   Handler get handler {
     Router router = Router();
+
+    //Listagem de Lojas
     router.get('/partners', (Request req) {
-      return Response.ok('List of Partners');
+      return Response.ok('Listagem de Lojas');
+    });
+
+    //Adicao de nova Loja
+    router.post('/partners', (Request req) {
+      return Response.ok('Adicao de nova Loja');
+    });
+
+    //Atualizacao de uma loja existente  através de Query Parameters
+    router.put('/partners', (Request req) {
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Objeto $id atualizado com sucesso');
+    });
+
+    //Delete de API
+    router.delete('/partners', (Request req) {
+      return Response.ok('Objeto deletado');
     });
 
     return router;
